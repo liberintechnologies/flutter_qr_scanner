41befd49d316423b8c4ead21e8148ab5e78c0e07
aksLt01 <arvind@liberintechnologies.com>
2023-01-03 11:42:18 +0530

    udpated app name

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:flutter_qr_scanner/qr_scanner.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:qr_scanner/qr_scanner.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
